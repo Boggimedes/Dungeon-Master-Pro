6ef66aff43f5b7048f977f2b8979e90e3bbe3ab6
Boggimedes <boggimedes@gmail.com>
2021-12-01 09:06:36 -0800

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,16 +86,12 @@ License: You must have a valid license purchased only from themeforest(the above
                             understanding spouse and daughter who started this project nearly a
                             decade ago. Back then this was primarily built in excel and was not
                             nearly so feature rich, but the concept originated from there. My goal
-                            was a system that made MY style of tabletop gaming easier. I say "my
-                            style" because there are a multitude of ways to play a tabletop RPG
-                            game, and the parts of the game that receive the focus can vary
-                            widely. Anyone who has been playing for a while has been in games
-                            where 90% of the session was spend on the combat board, and perhaps
-                            games where the combat board is only seen once every few sessions.
+                            was a system that made an adaptive storytelling style less work.
                         </p>
                         <p>
-                            What is "my style" you ask? I like to call it "Collaborative
-                            Storytelling". My focus is on not just telling a good story, but doing
+                            What is "adaptive storytelling" you ask? Another way to describe it
+                            would be "collabortive storytelling". My focus is on not just telling a good story, but
+                            doing
                             so in partner with the players. In order to facilitate that; my games
                             are far less scripted. I tend to prepare worlds/characters more than
                             specific sessions, and story arcs unfold based on the player actions,
@@ -105,10 +101,10 @@ License: You must have a valid license purchased only from themeforest(the above
                             storytelling.
                         </p>
                         <p>
-                            The goal of GMP is to provide infinite story/worldbuilding ideas, and
-                            help keep the mechanics of gamplay in the background. What makes GMP
-                            different than others that I have seen? Two words: NPC Generation. Ok,
-                            I guess that technically 4 words, but the point is GMP focuses on
+                            The goal of GMP is to provide infinite story/worldbuilding fodder, and
+                            help keep the mechanics of gamplay where they belong: in the background. What makes GMP
+                            different? Two words: NPC Generation. Ok, I guess that technically 4 words, but the point is
+                            GMP focuses on
                             generating interesting and unique NPCs with more backstory than some
                             player characters.
                         </p>
@@ -140,10 +136,28 @@ License: You must have a valid license purchased only from themeforest(the above
                     <div class="col-12">
                         <div class="card mt-5">
                             <div class="card-body">
-                                <video width="100%" height="auto" controls>
-                                    <source src="/assets/story-short.webm" type="video/webm" />
-                                    Your browser does not support the video tag.
-                                </video>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <h4>Create a Living World</h4>
+                                        <p>
+                                            NPCs aren't just generated, they are born, live lives, and have families.
+                                            Each region is seeded with NPC's then "Aged" a few
+                                            hundred or a few thousand years. Each year is "lived" by each NPC. They
+                                            get jobs, meet, marry, have children, and die. They pass on
+                                            traits like lineage (demonic, angelic, draconic), they occasionally
+                                            contract Lycanthropy, and their children are more likely to
+                                            follow in their footsteps (professions can run in families). If you have
+                                            Monarchies in your world, royal families will be created.
+                                            The system keeps up to 6 generations. When you need an NPC
+                                            you get physical traits, profession, family
+                                            relations, and so much more. Some NPCs could be a gaming session all by
+                                            themselves
+                                        </p>
+                                    </div>
+                                    <div class="col-9">
+                                        <img class="img-fluid" src="/assets/img/pages/story.jpg" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="card mt-5">
@@ -169,26 +183,10 @@ License: You must have a valid license purchased only from themeforest(the above
                         </div>
                         <div class="card mt-5">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-3">
-                                        <h4>Create a Living World</h4>
-                                        <p>
-                                            NPCs aren't just generated, they live lives and have families.
-                                            Each region is seeded with NPC's then "Aged" a few
-                                            hundred/thousand years. Each year is "lived" by each NPC. They
-                                            get jobs, meet, marry, have children, and die. They pass on
-                                            traits like demonic or angelic lineage, they occasionally
-                                            contract Lycanthropy, and their children are more likely to
-                                            follow in their footsteps (professions can run in families).
-                                            The system keeps up to 10 generations. When you need an NPC
-                                            you get a smattering of physical traits, profession, family
-                                            relations, and so much more.
-                                        </p>
-                                    </div>
-                                    <div class="col-9">
-                                        <img class="img-fluid" src="/assets/img/pages/story.jpg" />
-                                    </div>
-                                </div>
+                                <video width="100%" height="auto" controls>
+                                    <source src="/assets/story-short.webm" type="video/webm" />
+                                    Your browser does not support the video tag.
+                                </video>
                             </div>
                         </div>
                         <div class="card mt-5">
@@ -242,7 +240,7 @@ License: You must have a valid license purchased only from themeforest(the above
                         <h5>Timeline?</h5>
                         <p>
                             If there isn't much interest then my progress will be slower. I'm
-                            perfectly willing to use my product even if it's buggy, but if others
+                            perfectly willing to use my system even if it's buggy, but if others
                             are relying on it for their games I'll be more motivated.
                         </p>
                         <h5>
@@ -252,7 +250,7 @@ License: You must have a valid license purchased only from themeforest(the above
                         <ul>
                             <li>Campaign/Story sharing</li>
                             <li>Color/Pallet customization</li>
-                            <li>Timeline, plot points, and marker visibility</li>
+                            <li>Timeline, plot points, and POI visibility</li>
                             <li>Rule/System reference (perhaps editable for house rules)</li>
                             <li>Quick generate NPC class from selected class and level (including npcs in combat becomes
                                 a
@@ -263,7 +261,7 @@ License: You must have a valid license purchased only from themeforest(the above
                         <p>
                             Does a dragon hoard treasure? Ok, bad example because that does vary
                             by mythology. While this may be my side project, I am a Senior Full
-                            Stack developer. No shifty adventurers are getting this dragon.
+                            Stack developer. No shifty adventurers are getting by this dragon.
                         </p>
                     </div>
                 </div>
